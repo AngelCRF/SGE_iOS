f0ceb70a87c3797a3997540881e98e87c1696945
Kevin Erick <keam.crf@gmail.com>
2018-06-26 18:31:17 -0500

    Add some icons and functions

---
+++ SGE_iOS/ShowPostViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ShowPostViewController.swift
+//  SGE_iOS
+//
+//  Created by Kevin Angel on 16/06/18.
+//  Copyright © 2018 KEAM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShowPostViewController: UIViewController{
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.tintColor = UIColor(hexString: "#dc9c03")
+    }
+}
